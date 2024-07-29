3c08496bb19324bafe9b47be75415eb1e700346a
TestyDungeon <yura.bond.y@gmail.com>
2024-07-29 14:49:15 +0300

    Files

--- extensions/discord_music.py
+++ extensions/discord_music.py
@@ -7,6 +7,9 @@ import re
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
+from apiclient.discovery import build
+from pytube import YouTube
+
 
 # plugin = lightbulb.Plugin("Discord Music MP3 Downloader")
 
@@ -21,28 +24,22 @@ import googleapiclient.errors
 
 api_service_name = "youtube"
 api_version = "v3"
+api_key = "AIzaSyCn7WNc4uuVpbo-EiRtsgl_E9taCdHrAR4"
 
-url = "https://music.youtube.com/watch?v=dGzvyW4_T5U&si=uVTCLSeYn9-iHhvR"
-
+url = "https://music.youtube.com/watch?v=U-dv06NhGn8&si=vN4UVubAEfqJna3b"
 r = requests.get(url)
+
+youtube = build('youtube','v3',developerKey = api_key)
 
 soup = BeautifulSoup(r.content, 'lxml')
 content_values = [tag['content'] for tag in soup.find_all(attrs={"property" : "og:video:tag"})]
-song_names = re.sub(r'[^\w]', '+', str(content_values))
+song_names = re.sub(r'[^\w]', ' ', str(content_values))
 print(song_names)
 
-#html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + "despacito")
-#print(html.read().decode()[:70000])
+request = youtube.search().list(q=song_names, part='snippet')
+video_id = request.execute()["items"][0]["id"]["videoId"]
+video_link = "https://www.youtube.com/watch?v=" + video_id
+print(video_link)
 
-#soup2 = BeautifulSoup(html.read().decode()[:10000], 'html.parser')
-
-youtube = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
-
-request = youtube.search().list(q="exam")
-response = request.execute()
-
-print(response)
-
-# for link in soup2:
-#     if link.has_attr('href'):
-#         print(link['href'])
+video = YouTube(video_link)
+print(video.streams)
--- factorial.py
+++ factorial.py
@@ -1,21 +1,48 @@
 def romanToInt(s: str) -> int:
         int_value = 0
+        pre = 0
         for i in s:
             match i:
                 case "I":
                     int_value += 1
+                    print(int_value) 
                 case "V":
-                    int_value += 5
+                    if pre == "I":
+                        int_value += 3
+                    else:
+                        int_value += 5
+                    print(int_value)    
                 case "X":
-                    int_value += 10
+                    if pre == "I":
+                        int_value += 8
+                    else:
+                        int_value += 10
+                    print(int_value) 
                 case "L":
-                    int_value += 50
+                    if pre == "X":
+                        int_value += 30
+                    else:
+                        int_value += 50
+                    print(int_value) 
                 case "C":
-                    int_value += 100
+                    if pre == "X":
+                        int_value += 80
+                    else:
+                        int_value += 100
+                    print(int_value) 
                 case "D":
-                    int_value += 500
+                    if pre == "C":
+                        int_value += 300
+                    else:
+                        int_value += 500
+                    print(int_value) 
                 case "M":
-                    int_value += 1000
+                    if pre == "C":
+                        int_value += 800
+                    else:
+                        int_value += 1000
+                        print(int_value) 
+            pre = i
         return int_value
 
-print(romanToInt("IV"))        
+print(romanToInt("MCMXCIV"))        
